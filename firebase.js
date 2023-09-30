7c017291ed11eb8b8802aba68a48b0da2eb43e12
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-09-30 09:02:01 +0100

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDggLyp1fBmERH_iGiVSpDdqnVKpWLOA2w",
+  authDomain: "react-notes-115c6.firebaseapp.com",
+  projectId: "react-notes-115c6",
+  storageBucket: "react-notes-115c6.appspot.com",
+  messagingSenderId: "1010138231239",
+  appId: "1:1010138231239:web:cf1c97a882ea60053f69e2"
+};
+
+const app = initializeApp(firebaseConfig);
